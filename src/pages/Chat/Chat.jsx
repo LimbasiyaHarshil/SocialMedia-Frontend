343f76151ba7f7bdf9c40e86c24e976731b5c947
LimbasiyaHarshil <harshillimbasiya4505@gmail.com>
2025-05-15 12:15:06 +0530

    profile photo store

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -34,7 +34,7 @@ const Chat = () => {
 
   // Connect to Socket.io
   useEffect(() => {
-    socket.current = io("ws://localhost:8800");
+    socket.current = io(process.env.REACT_APP_SOCKET_URL || "ws://localhost:8800");
     socket.current.emit("new-user-add", user._id);
     socket.current.on("get-users", (users) => {
       setOnlineUsers(users);
